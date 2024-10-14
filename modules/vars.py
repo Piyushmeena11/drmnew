5c61fe45db5d0f6ff0d99a137afa3a1ec4458fe1
Piyushmeena11 <meenapiyush23@gmail.com>
2024-10-14 16:16:47 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
-api_id = 28829496
-api_hash = "8094341dbe5bd50090de8b12a01d04b8"
-bot_token = "6715326658:AAHVLu_V2HJKquZG4kAPydJDP6G1lATc8lA"
+api_id = "27309858"
+api_hash = "db2420265b9b2e6602f487fa3f102ba6"
+bot_token = "7954604868:AAHCMaOzlRCurV3TRlnsatvS2o_VSxVQMW8"
 # Bot Username = "allinone"
